2839fc88726ddfe4fcb2890594a544a616125f1b
gilbianmaytie <56081889+gilbianmaytie@users.noreply.github.com>
2024-04-23 19:25:20 +0700

    Aupdate data

--- main.go
+++ main.go
@@ -1,29 +1,50 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"runtime"
-	"time"
 )
 
-func main() {
-	// Get OS information
-	fmt.Println("OS:", runtime.GOOS)
-	fmt.Println("Architecture:", runtime.GOARCH)
-	fmt.Println("Number of CPUs:", runtime.NumCPU())
+type OSInfo struct {
+	OS         string `json:"os"`
+	Arch       string `json:"arch"`
+	NumCPU     int    `json:"numCPU"`
+	CPUUsage   string `json:"cpuUsage"`
+	GoVersion  string `json:"goVersion"`
+	NumThreads int    `json:"numThreads"`
+}
 
-	// Print CPU usage every second
-	for {
-		cpuUsage := calculateCPUUsage()
-		fmt.Printf("CPU Usage: %.2f%%\n", cpuUsage)
-		time.Sleep(time.Second)
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		info := OSInfo{
+			OS:         runtime.GOOS,
+			Arch:       runtime.GOARCH,
+			NumCPU:     runtime.NumCPU(),
+			CPUUsage:   calculateCPUUsage(),
+			GoVersion:  runtime.Version(),
+			NumThreads: runtime.NumGoroutine(),
+		}
+		jsonResponse, err := json.Marshal(info)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(jsonResponse)
+	})
+
+	fmt.Println("Server listening on port 8080...")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println(err)
 	}
 }
 
-func calculateCPUUsage() float64 {
-	// Get CPU usage
+func calculateCPUUsage() string {
 	var stat runtime.MemStats
 	runtime.ReadMemStats(&stat)
 	cpuUsage := float64(stat.Sys-stat.HeapReleased) / float64(stat.Sys) * 100
-	return cpuUsage
+	return fmt.Sprintf("%.2f%%", cpuUsage)
 }
